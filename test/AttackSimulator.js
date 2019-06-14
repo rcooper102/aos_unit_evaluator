468b3d759c44eb6db59dd7fea0f40908091cd0c7
Ryan Cooper <ryan.cooper@groupbyinc.com>
2019-06-14 19:43:08 -0400

    installed mocha/chai

---
+++ test/AttackSimulator.js
@@ -0,0 +1,14 @@
+import { AttackSimulator } from "../src/services/simulator/AttackSimulator.js";
+const assert = require('assert');
+const { it } = require('mocha');
+const { expect } = require('chai');
+
+
+
+describe('AttackSimulator', function () {	
+ 	it('Should simulate statistical average for simple attacks', function () {
+        expect(true).to.equal(true);
+    });
+
+
+});
