483ee22732e80c6f8a99c5e41a1a798e570b9769
Ryan Cooper <ryan.cooper@groupbyinc.com>
2019-06-27 10:31:32 -0400

    transpiling to es5 now because edge is stupid.

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,11 @@ module.exports = {
 				use: ['css-loader', 'sass-loader']
 			})
 		},
+		{
+	      test: /\.js$/,
+	      exclude: /node_modules/,
+	      loader: 'babel-loader',
+	    }
 	]
 	},
 	plugins: [ 
