c0772656edb84429945ef84ae7db9026718b007a
Ryan Cooper <ryan@terminalvelocity.ca>
2019-08-07 14:16:02 -0400

    Minor tweaks to adjust layout bugs on all results page (#12)
    * tweaks Removed score decimal point.
    
    * tweaks Fixed layout of all results.

--- src/components/results/AllResults.js
+++ src/components/results/AllResults.js
@@ -11,6 +11,16 @@ export class AllResults extends Base {
 		this.make("results");
 		this.addClass("all-results");
 
+		const title = new Header(3);
+		title.text = Locale.gen("all-histogram-title");
+		this.addChild(title);
+
+		const sub = new Base();
+		sub.make("rolls");
+		sub.text = Locale.gen("all-histogram-sub-title");
+		this.addChild(sub);
+
+
 		const histogram = new AllHistogram();
 		this.addChild(histogram);
 		histogram.update(data);	
