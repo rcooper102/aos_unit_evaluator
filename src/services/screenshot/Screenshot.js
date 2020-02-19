ce75310ef69eeaa2ed057b8462a55e45690bc855
Ryan Cooper <ryan.cooper@groupbyinc.com>
2020-02-19 10:24:50 -0500

    share-features Made results screenshot work

--- src/services/screenshot/Screenshot.js
+++ src/services/screenshot/Screenshot.js
@@ -2,25 +2,53 @@ const html2Canvas = require("html2canvas");
 
 export class Screenshot extends EventDispatcher {
 
-	constructor (node, name = "screen.png") {
+	static get MODES() {
+		return  {
+			RESULTS: { query: "results", fix: Screenshot.fixResults, cleanup: Screenshot.cleanResults },
+		};
+	}
+
+	constructor (mode, name = "screen.jpg") {
 		super();
+
+		if(mode) {
+			mode.fix();
+		}
 
 		const location = document.body;
 
-		html2Canvas(node).then((canvas) => {
+		html2Canvas(document.querySelector(mode.query), {
+        	scrollX: 0,
+        	scrollY: 0,
+      	}).then((canvas) => {
 		    location.appendChild(canvas);
 
 		    setTimeout(() => {
 				const link = document.createElement('a');
 				location.appendChild(link);
 				link.setAttribute('download', name);
-				link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+				link.setAttribute('href', canvas.toDataURL("image/jpg").replace("image/jpg", "image/octet-stream"));
 				link.click();
 
 				location.removeChild(canvas);
 				location.removeChild(link);
+
+				if(mode) {
+					mode.cleanup();
+				}
+
 				this.dispatch(new Event(Event.COMPLETE, this));
 			}, 1);
 		});
+	}
+
+	static fixResults() {
+		document.body.style.overflow = "auto";
+		document.querySelector("results").style.height = "auto";
+	}
+
+	static cleanResults() {
+		document.body.style.overflow = "hidden";
+		document.querySelector("results").style.height = "calc(100vh - 69px)";
 	}
 }
